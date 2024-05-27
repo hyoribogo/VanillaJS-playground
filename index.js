13959a2857713d7c9b5b802f18caa167bec09413
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-05-27 18:16:21 +0900

    chore: 기본 세팅

---
+++ index.js
@@ -0,0 +1,5 @@
+import App from './src/App'
+
+const $root = document.querySelector('#root')
+
+new App($root)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+class App {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    console.log('App initialized')
+  }
+}
+
+export default App
